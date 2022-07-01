16322c7f0d91815b75371439f8ae24e07bd00e2d
neglux <akcaaytuna@gmail.com>
2022-07-01 19:35:00 +0300

    develop: implementation of mergesort

---
+++ mergeSort.js
@@ -0,0 +1,41 @@
+function mergeSort(array) {
+  if (array.length < 2) return;
+  const mid = Math.trunc(array.length / 2);
+  const leftArray = array.slice(0, mid);
+  const rightArray = array.slice(mid, array.length);
+
+  mergeSort(leftArray);
+  mergeSort(rightArray);
+  merge(leftArray, rightArray, array);
+  return array;
+}
+
+function merge(leftArray, rightArray, array) {
+  let i = 0,
+    j = 0,
+    k = 0;
+  while (i < leftArray.length && j < rightArray.length) {
+    if (leftArray[i] <= rightArray[j]) {
+      array[k] = leftArray[i];
+      i++;
+    } else {
+      array[k] = rightArray[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < leftArray.length) {
+    array[k] = leftArray[i];
+    i++;
+    k++;
+  }
+
+  while (j < rightArray.length) {
+    array[k] = rightArray[j];
+    j++;
+    k++;
+  }
+}
+
+const array = mergeSort([1, 3, 7, 4, 2, 5, 8, 6, 9, 0]);
